fb71387c549a595a8eb7bd721d57475cfb673e20
Bryan Boreham <bjboreham@gmail.com>
2018-03-02 19:24:08 +0000

    WIP, program actually runs

--- cmd/analyzer/main.go
+++ cmd/analyzer/main.go
@@ -4,8 +4,10 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/go-kit/kit/log/level"
+	"github.com/prometheus/common/model"
 
 	"github.com/weaveworks/cortex/pkg/chunk"
 	"github.com/weaveworks/cortex/pkg/chunk/storage"
@@ -31,18 +33,27 @@ func main() {
 	}
 
 	chunkStore, err := chunk.NewStore(chunkStoreConfig, schemaConfig, storageClient)
+	checkFatal(err)
+	defer chunkStore.Stop()
+
+	if flag.NArg() != 1 {
+		level.Error(util.Logger).Log("usage: analyzer <options> timestamp")
+	}
+
+	ts, err := time.Parse(time.RFC3339, flag.Arg(0))
+	checkFatal(err)
+	mt := model.TimeFromUnixNano(ts.UnixNano())
+
+	err = chunk.Analyze(chunkStore, schemaConfig, mt, mt, "2")
+	checkFatal(err)
+
+	//fmt.Printf("result: error %s %s\n", result.Err, result.Value)
+	fmt.Printf("Done\n")
+}
+
+func checkFatal(err error) {
 	if err != nil {
 		level.Error(util.Logger).Log("err", err)
 		os.Exit(1)
 	}
-	defer chunkStore.Stop()
-
-	if flag.NArg() != 0 {
-		level.Error(util.Logger).Log("usage: analyzer <options>")
-	}
-
-	chunk.Analyze()
-
-	//fmt.Printf("result: error %s %s\n", result.Err, result.Value)
-	fmt.Printf("Done\n")
 }
--- pkg/chunk/aws/analyzer.go
+++ pkg/chunk/aws/analyzer.go
@@ -1,24 +1,46 @@
-package chunk
+package aws
 
 import (
+	"context"
 	"fmt"
 
-	"github.com/prometheus/common/model"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
 
-func Analyze(from, through model.Time, userID string) {
-	fmt.Printf("Start\n")
+func (a storageClient) AnalyzeBucket(tableName, bucketHash string) error {
+	ctx := context.TODO()
 
 	input := &dynamodb.QueryInput{
-		TableName: aws.String(query.TableName),
+		TableName: aws.String(tableName),
 		KeyConditions: map[string]*dynamodb.Condition{
 			hashKey: {
 				AttributeValueList: []*dynamodb.AttributeValue{
-					{S: aws.String(query.HashValue)},
+					{S: aws.String(bucketHash)},
 				},
 				ComparisonOperator: aws.String(dynamodb.ComparisonOperatorEq),
 			},
 		},
-		ReturnConsumedCapacity: aws.String(dynamodb.ReturnConsumedCapacityTotal),
 	}
+
+	count := 0
+
+	request := a.queryRequest(ctx, input)
+
+	for page := request; page != nil; page = page.NextPage() {
+		resp, err := a.queryPage(ctx, input, page)
+		if err != nil {
+			return err
+		}
+		fmt.Printf("%d entries returned\n", resp.Len())
+		for i := 0; i < resp.Len(); i++ {
+			fmt.Printf("%v: %v\n", resp.RangeValue(i), resp.Value(i))
+			count++
+			if count > 100 {
+				return nil
+			}
+		}
+	}
+
+	return nil
 }
