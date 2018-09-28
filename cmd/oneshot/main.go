efd92a7e6a23300508d826167ba72c9e34d7a92b
Bryan Boreham <bjboreham@gmail.com>
2018-09-28 15:16:46 +0000

    Update for Cortex API changes

--- cmd/oneshot/main.go
+++ cmd/oneshot/main.go
@@ -7,16 +7,16 @@ import (
 	"os"
 	"time"
 
+	"github.com/go-kit/kit/log/level"
 	"github.com/prometheus/common/model"
 	"github.com/prometheus/prometheus/pkg/labels"
-	"github.com/weaveworks/cortex/pkg/prom1/storage/metric"
-
-	"github.com/go-kit/kit/log/level"
-	"github.com/prometheus/prometheus/promql"
-
+	"github.com/weaveworks/common/logging"
 	"github.com/weaveworks/common/user"
+
 	"github.com/weaveworks/cortex/pkg/chunk"
 	"github.com/weaveworks/cortex/pkg/chunk/storage"
+	"github.com/weaveworks/cortex/pkg/ingester/client"
+	"github.com/weaveworks/cortex/pkg/prom1/storage/metric"
 	"github.com/weaveworks/cortex/pkg/querier"
 	"github.com/weaveworks/cortex/pkg/util"
 )
@@ -26,35 +26,38 @@ func main() {
 		chunkStoreConfig chunk.StoreConfig
 		schemaConfig     chunk.SchemaConfig
 		storageConfig    storage.Config
-		logLevel         util.LogLevel
+		querierConfig    querier.Config
+		loglevel         string
 	)
-	util.RegisterFlags(&chunkStoreConfig, &schemaConfig, &storageConfig, &logLevel)
+	util.RegisterFlags(&chunkStoreConfig, &schemaConfig, &storageConfig, &querierConfig)
+	flag.StringVar(&loglevel, "log-level", "info", "Debug level: debug, info, warning, error")
 	flag.Parse()
 
-	util.InitLogger(logLevel.AllowedLevel)
+	var l logging.Level
+	l.Set(loglevel)
+	util.Logger, _ = util.NewPrometheusLogger(l)
 
-	storageClient, err := storage.NewStorageClient(storageConfig, schemaConfig)
+	storageOpts, err := storage.Opts(storageConfig, schemaConfig)
 	if err != nil {
 		level.Error(util.Logger).Log("msg", "error initializing storage client", "err", err)
 		os.Exit(1)
 	}
 
-	chunkStore, err := chunk.NewStore(chunkStoreConfig, schemaConfig, storageClient)
+	chunkStore, err := chunk.NewStore(chunkStoreConfig, schemaConfig, storageOpts)
 	if err != nil {
 		level.Error(util.Logger).Log("err", err)
 		os.Exit(1)
 	}
 	defer chunkStore.Stop()
 
-	sampleQueryable := querier.NewQueryable(noopQuerier{}, chunkStore, false)
-	engine := promql.NewEngine(sampleQueryable, nil)
+	queryable, engine := querier.New(querierConfig, noopDistributor{}, chunkStore)
 
 	if flag.NArg() != 1 {
 		level.Error(util.Logger).Log("usage: oneshot <options> promql-query")
 	}
 
 	// Now execute the query
-	query, err := engine.NewInstantQuery(flag.Arg(0), time.Now().Add(-time.Hour*24))
+	query, err := engine.NewInstantQuery(queryable, flag.Arg(0), time.Now().Add(-time.Hour*24))
 	if err != nil {
 		level.Error(util.Logger).Log("error in query:", err)
 		os.Exit(1)
@@ -65,16 +68,20 @@ func main() {
 	fmt.Printf("result: error %s %s\n", result.Err, result.Value)
 }
 
-type noopQuerier struct{}
+type noopDistributor struct{}
 
-func (n noopQuerier) Query(ctx context.Context, from, to model.Time, matchers ...*labels.Matcher) (model.Matrix, error) {
+func (n noopDistributor) Query(ctx context.Context, from, to model.Time, matchers ...*labels.Matcher) (model.Matrix, error) {
 	return nil, nil
 }
 
-func (n noopQuerier) MetricsForLabelMatchers(ctx context.Context, from, through model.Time, matchers ...*labels.Matcher) ([]metric.Metric, error) {
+func (n noopDistributor) MetricsForLabelMatchers(ctx context.Context, from, through model.Time, matchers ...*labels.Matcher) ([]metric.Metric, error) {
 	return nil, nil
 }
 
-func (n noopQuerier) LabelValuesForLabelName(context.Context, model.LabelName) (model.LabelValues, error) {
+func (n noopDistributor) LabelValuesForLabelName(context.Context, model.LabelName) ([]string, error) {
+	return nil, nil
+}
+
+func (n noopDistributor) QueryStream(ctx context.Context, from, to model.Time, matchers ...*labels.Matcher) ([]client.TimeSeriesChunk, error) {
 	return nil, nil
 }
