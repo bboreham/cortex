de0105c43c22535ab8cf03696757dfe0e17b4a19
Bryan Boreham <bryan@weave.works>
2018-10-04 14:03:13 +0000

    Return number of items in Take()
    because the batch can be smaller after deduplication

--- pkg/chunk/gcp/storage_client.go
+++ pkg/chunk/gcp/storage_client.go
@@ -192,7 +192,7 @@ func (b bigtableWriteBatch) AddBatch(a chunk.WriteBatch) {
 	}
 }
 
-func (b bigtableWriteBatch) Take(undersizedOK bool) chunk.WriteBatch {
+func (b bigtableWriteBatch) Take(undersizedOK bool) (chunk.WriteBatch, int) {
 	ret := bigtableWriteBatch{
 		tables: map[string]map[string]*bigtable.Mutation{},
 	}
@@ -210,7 +210,7 @@ func (b bigtableWriteBatch) Take(undersizedOK bool) chunk.WriteBatch {
 			delete(fromRows, rowKey)
 			count++
 			if count >= batchSize {
-				return ret
+				return ret, ret.Len()
 			}
 		}
 		if len(fromRows) == 0 {
@@ -218,7 +218,7 @@ func (b bigtableWriteBatch) Take(undersizedOK bool) chunk.WriteBatch {
 		}
 	}
 
-	return ret
+	return ret, ret.Len()
 }
 
 func (s *storageClientColumnKey) BatchWrite(ctx context.Context, batch chunk.WriteBatch) error {
