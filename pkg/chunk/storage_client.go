2a1fbd9e53ae5bdb2315744754332687856cef5c
Bryan Boreham <bryan@weave.works>
2019-04-03 09:14:14 +0100

    WIP scan a table

--- pkg/chunk/storage_client.go
+++ pkg/chunk/storage_client.go
@@ -5,6 +5,8 @@ import (
 	"errors"
 	"io"
 	"time"
+
+	"github.com/prometheus/common/model"
 )
 
 // DirDelim is the delimiter used to model a directory structure in an object store.
@@ -41,6 +43,12 @@ type Client interface {
 // ObjectAndIndexClient allows optimisations where the same client handles both
 type ObjectAndIndexClient interface {
 	PutChunkAndIndex(ctx context.Context, c Chunk, index WriteBatch) error
+}
+
+// ObjectClient2 because naming is hard
+type ObjectClient2 interface {
+	ObjectClient
+	Scan(ctx context.Context, from, through model.Time, withValue bool, callbacks []func(result ReadBatch)) error
 }
 
 // WriteBatch represents a batch of writes.
