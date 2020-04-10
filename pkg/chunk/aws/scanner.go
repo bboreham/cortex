8e8df6078760b261f20e41d6d531215953989336
Bryan Boreham <bryan@weave.works>
2020-04-10 13:42:07 +0000

    Run table scan 10 segments at a time, to improve restartability

--- pkg/chunk/aws/scanner.go
+++ pkg/chunk/aws/scanner.go
@@ -9,10 +9,14 @@ import (
 	"github.com/aws/aws-sdk-go/aws/client"
 	"github.com/aws/aws-sdk-go/aws/request"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/go-kit/kit/log/level"
 	"github.com/prometheus/common/model"
 
 	"github.com/cortexproject/cortex/pkg/chunk"
+	"github.com/cortexproject/cortex/pkg/util"
 )
+
+const gangSize = 10
 
 // ScanTable reads the whole of a table on multiple goroutines in
 // parallel, calling back with batches of results on one of the
@@ -27,39 +31,50 @@ func (a dynamoDBStorageClient) Scan(ctx context.Context, from, through model.Tim
 	if withValue {
 		projection += "," + valueKey
 	}
-	var readerGroup sync.WaitGroup
-	readerGroup.Add(len(callbacks))
-	for segment, callback := range callbacks {
-		go func(segment int, callback func(result chunk.ReadBatch)) {
-			input := &dynamodb.ScanInput{
-				TableName:              aws.String(tableName),
-				ProjectionExpression:   aws.String(projection),
-				Segment:                aws.Int64(int64(segment)),
-				TotalSegments:          aws.Int64(int64(len(callbacks))),
-				ReturnConsumedCapacity: aws.String(dynamodb.ReturnConsumedCapacityTotal),
-			}
-			withRetrys := func(req *request.Request) {
-				req.Retryer = client.DefaultRetryer{NumMaxRetries: a.cfg.backoffConfig.MaxRetries}
-			}
-			err := a.DynamoDB.ScanPagesWithContext(ctx, input, func(page *dynamodb.ScanOutput, lastPage bool) bool {
-				if cc := page.ConsumedCapacity; cc != nil {
-					dynamoConsumedCapacity.WithLabelValues("DynamoDB.ScanTable", *cc.TableName).
-						Add(float64(*cc.CapacityUnits))
+	for startSegment := 0; startSegment < len(callbacks); startSegment += gangSize {
+		numToRun := len(callbacks) - startSegment
+		if numToRun > gangSize {
+			numToRun = gangSize
+		}
+		level.Info(util.Logger).Log("msg", "Starting scan", "tableName", tableName, "startSegment", startSegment, "numToRun", numToRun)
+		var readerGroup sync.WaitGroup
+		readerGroup.Add(numToRun)
+		for i, callback := range callbacks[startSegment : startSegment+numToRun] {
+			go func(segment int, callback func(result chunk.ReadBatch)) {
+				input := &dynamodb.ScanInput{
+					TableName:              aws.String(tableName),
+					ProjectionExpression:   aws.String(projection),
+					Segment:                aws.Int64(int64(segment)),
+					TotalSegments:          aws.Int64(int64(len(callbacks))),
+					ReturnConsumedCapacity: aws.String(dynamodb.ReturnConsumedCapacityTotal),
 				}
+				withRetrys := func(req *request.Request) {
+					req.Retryer = client.DefaultRetryer{NumMaxRetries: a.cfg.backoffConfig.MaxRetries}
+				}
+				err := a.DynamoDB.ScanPagesWithContext(ctx, input, func(page *dynamodb.ScanOutput, lastPage bool) bool {
+					if cc := page.ConsumedCapacity; cc != nil {
+						dynamoConsumedCapacity.WithLabelValues("DynamoDB.ScanTable", *cc.TableName).
+							Add(float64(*cc.CapacityUnits))
+					}
 
-				callback(&dynamoDBReadResponse{items: page.Items})
-				return true
-			}, withRetrys)
-			if err != nil {
-				outerErr = err
-				// TODO: abort all segments
-			}
-			readerGroup.Done()
-		}(segment, callback)
+					callback(&dynamoDBReadResponse{items: page.Items})
+					return true
+				}, withRetrys)
+				if err != nil {
+					outerErr = err
+					// TODO: abort all segments
+				}
+				level.Info(util.Logger).Log("msg", "Segment finished", "segment", segment)
+				readerGroup.Done()
+			}(startSegment+i, callback)
+		}
+		// Wait until all reader segments have finished
+		readerGroup.Wait()
+		if outerErr != nil {
+			return outerErr
+		}
 	}
-	// Wait until all reader segments have finished
-	readerGroup.Wait()
-	return outerErr
+	return nil
 }
 
 func (a s3ObjectClient) Scan(ctx context.Context, from, through model.Time, withValue bool, callbacks []func(result chunk.ReadBatch)) error {
